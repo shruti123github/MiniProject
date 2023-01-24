8fd099b85264e8a95eadf3ca949da07b26aec3b7
shruti <jadhav.shruti2103@gmail.com>
2023-01-24 21:33:41 +0530

    Project Created

---
+++ src/com/velocity/Mini.java
@@ -0,0 +1,16 @@
+package com.velocity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Mini {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        List<String> l=new ArrayList<String>();
+        l.add("java");
+        l.add("spring");
+        l.add("hibernet");
+        System.out.println(l);
+        
+	}
+}
